6fa5f75d312ac86f87d231c511fede9cfb2cd842
DESKTOP-RRV5BLJ\qukoucai001 <2929094294@qq.com>
2018-11-22 16:32:06 +0800

    Revert "改名为MsRoom..."
    This reverts commit e9d73ea4e9eb1358528e85422a8d150a49c3c39f.

--- src/main/java/com/ecjtu/demo/dao/MsRoomMapper.java
+++ src/main/java/com/ecjtu/demo/dao/MsRoomMapper.java
@@ -3,5 +3,15 @@ package com.ecjtu.demo.dao;
 import com.ecjtu.demo.entity.MsRoom;
 
 public interface MsRoomMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(MsRoom record);
+
+    int insertSelective(MsRoom record);
+
     MsRoom selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(MsRoom record);
+
+    int updateByPrimaryKey(MsRoom record);
 }
--- src/main/java/com/ecjtu/demo/web/AreaController.java
+++ src/main/java/com/ecjtu/demo/web/AreaController.java
@@ -1,20 +1,27 @@
 package com.ecjtu.demo.web;
 
+import com.ecjtu.demo.entity.Area;
 import com.ecjtu.demo.entity.MsRoom;
-import com.ecjtu.demo.service.MsRoomService;
+import com.ecjtu.demo.service.AreaService;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/superadmin")
-public class MsRoomController {
+public class AreaController {
     @Autowired
-    private MsRoomService msRoomService;
+    private AreaService areaService;
 
     /**
      * 获取所有的区域信息
@@ -24,7 +31,7 @@ public class MsRoomController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     private Map<String, Object> listArea() {
         Map<String, Object> modelMap = new HashMap<String, Object>();
-        MsRoom msRoom= msRoomService.selectByPrimaryKey(16L);
+        MsRoom msRoom= areaService.selectByPrimaryKey(16L);
         modelMap.put("msRoom", msRoom);
         return modelMap;
     }
